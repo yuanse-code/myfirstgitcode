297bf30f80863b786f3e331e33ccc247ad5068d5
yuanse-code <3067541611@qq.com>
2024-08-25 11:40:32 +0800

    提交代码

---
+++ homework-9-15/night-9-15.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include "º¯ÊıÉùÃ÷.h"
+int main()
+{
+	int a = 10;
+	int b = 20;
+	printf("%d ", ADD(a, b));
+	return 0;
+}
---
+++ homework-9-15/函数声明.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int ADD(int x, int y)
+{
+	int c = 0;
+	c = x + y;
+	return c;
+}
