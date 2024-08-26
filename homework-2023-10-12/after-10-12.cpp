37746411e79833ceff1b36d8bbbcb8f46f3ad1f6
yuanse-code <3067541611@qq.com>
2024-08-26 12:46:21 +0800

    提交代码

---
+++ homework-2023-10-12/after-10-12.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<windows.h>
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
