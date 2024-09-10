2cb2a0981cac80877de111d71ca63ac34fb0d580
yuanse-code <3067541611@qq.com>
2024-09-10 17:13:56 +0800

    提交代码

---
+++ homework-2023-11-7/afternoon-11-7.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	int a=5 / 2;
+//	printf("a=%d \n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = -1;
+//	//--右移操作符
+//	// 移动二进制位
+//	//0000000000001000
+//	int b=a >> 1;
+//	printf("a=%d \n",sizeof(int));
+//	printf("a=%d \n", b);
+//	return 0;
+//}
+int main()
+{
+	int a = 5;
+	int b = a << 1;
+	int c = 0;
+	c = 1;
+	//左移操作符
+	printf("a=%d \n", b);
+	return 0;
+	if (!c)
+	{
+		printf("%d\n", a);
+	}
+}
