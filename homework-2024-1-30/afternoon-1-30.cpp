c2d981a7e1051c87bc41eb21c211f5036946ce11
yuanse-code <3067541611@qq.com>
2024-10-03 15:36:09 +0800

    提交代码

---
+++ homework-2024-1-30/afternoon-1-30.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <Windows.h>
+
+//int main()
+//{
+//	int i = 0;
+//	/*int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0;i <= 12;i++)
+//	{
+//		printf("hehe\n");
+//		arr[i] = 0;
+//	}*/
+//	for (i = 0;i < 100;i++)
+//	{
+//		printf("%d ", i);
+//	}
+//	for (i = 0;i < 100;i++)
+//	{
+//		printf("%d ", 10-i);
+//	}
+//	return 0;
+//}
+int Add(int x, int y)
+{
+	return x + y;
+}
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 20;
+//	int b = 10;
+//	int c = Add(a, b);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0;i < 10;i++);
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
