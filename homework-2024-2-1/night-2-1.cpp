c8cfbcbdd87936ac1dca90bc9feec4f897639b47
yuanse-code <3067541611@qq.com>
2024-10-05 19:41:15 +0800

    提交代码

---
+++ homework-2024-2-1/night-2-1.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <Windows.h>
+
+//void test2()
+//{
+//	printf("hehe\n");
+//}
+//void test1()
+//{
+//	test2();
+// }
+//void test()
+//{
+//	test1();
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+int main()
+{
+	int i = 0;
+	int arr[10] = { 0 };
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	system("pause");
+	return 0;
+}
