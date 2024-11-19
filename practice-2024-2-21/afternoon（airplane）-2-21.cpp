64667e2b8ef68a52b5487eeef0ce607cbba7f7a1
yuanse-code <3067541611@qq.com>
2024-11-19 10:14:49 +0800

    提交代码

---
+++ practice-2024-2-21/afternoon（airplane）-2-21.cpp
@@ -0,0 +1,12 @@
+#include "airplay.h"
+
+int main()
+{
+	//char a = 'a';
+	//a = _getch();//会刷新
+	//if(a=='a')
+	//printf("%c\n",'a');
+	//system("pause");
+	//游戏开始函数
+	return 0;
+}
---
+++ practice-2024-2-21/airplay.h
@@ -0,0 +1,7 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+
+//打开窗口，加载图片,
