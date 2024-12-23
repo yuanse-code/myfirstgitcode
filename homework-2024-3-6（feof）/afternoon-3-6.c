8375ff06d8808977d6053141ebf7c644a373bdb1
yuanse-code <3067541611@qq.com>
2024-12-23 10:13:21 +0800

    提交代码

---
+++ homework-2024-3-6（feof）/afternoon-3-6.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "D:/vsc++代码/mine.h"
+
+//int main()
+//{
+//	//EOF
+//	//feof();//EOF-文件结束标志
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//		return 0;
+//	int ch = fgetc(pf);
+//	printf("%d\n", ch);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+int main()
+{
+	//strerror
+	//perror
+	FILE* pf = fopen("test.txt","r");
+	if (pf == NULL)
+	{
+		perror("open file test2.txt");
+		return 0;
+	}
+	//读文件
+	int ch = 0;
+	while ((ch = fgetc(pf)) != EOF)
+	{
+		putc(ch,stdout);
+	}
+	if (ferror(pf))
+	{
+		printf("error\n");
+	}
+	else if (feof(pf))
+	{
+		printf("end of file\n");
+	}
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
